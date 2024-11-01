5e55d1cfca5b21b1a4166bf9f435a8f2ea041c54
Danielle <danielle@makeitfable.com>
2024-11-01 15:58:29 -0400

    Update front end

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: 'Anakin Flywalker', lastLogin: 'August 13, 2024' },
+    { name: 'Anakin Flywalker', lastLogin: 'August 13, 2024' },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="table">
+          <div className="table__heading">
+              <div className="table__row">
+                  <div>Name</div>
+                  <div>Last login</div>
+              </div>
+          </div>
+          <div>
+            {users.map((user, index) => (
+              <div className="table__row" key={index}>
+                <div>{user.name}</div>
+                <div>{user.lastLogin}</div>
+              </div>
+            ))}
+          </div>
+      </div>
     </div>
   );
 }
