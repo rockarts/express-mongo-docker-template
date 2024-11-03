ef1bbd613282436d0e9ca4551fe09cc94bdd469e
ptrin <perrytrinier+github@gmail.com>
2024-11-03 16:15:11 +0000

    WIP

--- api/controllers/communityMetrics.js
+++ api/controllers/communityMetrics.js
@@ -1,12 +1,29 @@
-
-const mongoose = require("mongoose")
-const schema = new mongoose.Schema({ firstName: String, lastName: String });
-const User = mongoose.model('User', schema);
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = new Schema({ firstName: String, lastName: String });
+const engagementSummarySchema = new Schema({
+  tester: {
+    ref: "User",
+    type: Schema.Types.ObjectId,
+  },
+  eligibleRequestsCount: Number,
+  notifiedRequestsCount: Number,
+  acceptedRequestsCount: Number,
+  logins: Number,
+  hoursWorked: Number,
+  startDate: Date,
+  endDate: Date,
+});
+const User = mongoose.model("User", userSchema);
+const EngagmentSummary = mongoose.model(
+  "EngagementSummary",
+  engagementSummarySchema,
+  "testerEngagementSummaries"
+);
 
 module.exports = {
   index: async (req, res) => {
     const users = await User.find({});
-    console.log(users);
     return res.json(users);
-  }
+  },
 };
